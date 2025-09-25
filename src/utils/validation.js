2ae0360a4467a6ed01f2d699c5104e2b23ef9cba
Azhdar <azhdar.aliev098@gmail.com>
2025-09-25 17:08:41 +0300

    исправил валидацию email

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -9,7 +9,7 @@ export const loginSchema = object({
 export const registerSchema = object({
   name: string().required('Имя обязательное поле'),
   login: string().required('Логин обязательное поле'),
-  email: string().email('Email обязательное поле'),
+  email: string().email().required('Email обязательное поле'),
   password: string().required('Пороль обязательное поле'),
   repeatPassword: string().required('Пороль обязательное поле'),
   checkbox: boolean().isTrue('Согласие с условиями обязательное поле'),
